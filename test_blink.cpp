226807f606378a4e4ea3073e8391f1cc858c6271
j-klenk <77705398+j-klenk@users.noreply.github.com>
2026-05-28 19:36:21 -0400

    Blink onboard LED via CYW43

--- test_blink.cpp
+++ test_blink.cpp
@@ -1,14 +1,22 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-
-
+#include "pico/cyw43_arch.h"
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    // Initialize the CYW43 driver (needed to control the onboard LED)
+    if (cyw43_arch_init())
+    {
+        printf("CYW43 init failed\n");
+        return -1;
+    }
+
+    while (true)
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // LED on
+        sleep_ms(500);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0); // LED off
+        sleep_ms(500);
     }
 }
