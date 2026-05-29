f80450b8a8293b0e838afeff704e61581412a95e
j-klenk <77705398+j-klenk@users.noreply.github.com>
2026-05-28 21:00:00 -0400

    Add button input controlling external LED

--- test_blink.cpp
+++ test_blink.cpp
@@ -1,17 +1,23 @@
 #include "pico/stdlib.h"
 
 #define LED_PIN 15
+#define BUTTON_PIN 14
 
 int main()
 {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
     while (true)
     {
-        gpio_put(LED_PIN, 1); // LED on
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0); // LED off
-        sleep_ms(500);
+        bool button_pressed = !gpio_get(BUTTON_PIN);
+
+        gpio_put(LED_PIN, button_pressed);
+
+        sleep_ms(10);
     }
 }
